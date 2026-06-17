898f8ca8ff3e7c353fcd762162bac1308a1b839e
Felipe De Andrade <felipe.deam98@gmail.com>
2026-06-17 06:00:46 -0300

    refactor: standardize project language to English

--- src/assets/js/lista.js
+++ src/assets/js/lista.js
@@ -1,25 +1,27 @@
-import lista from './mocks/pets.js';
+import lista from "./mocks/pets.js";
 
-const conteinerPets = document.querySelector('[data-lista-pets]');
+const conteinerPets = document.querySelector("[data-lista-pets]");
 
-const listaCompleta = lista.map((item) => {
-  return `<li class="pets_item">
-            <img class="pets_imagem" src="./assets/img/pets/${item.imagem}" width="650" height="650" alt="${item.descricao}">
-            <div class="pets_dados">
+const listaCompleta = lista
+  .map((item) => {
+    return `<li class="pets__item">
+            <img class="pets__image" src="./assets/img/pets/${item.imagem}" width="650" height="650" alt="${item.descricao}">
+            <div class="pets__data">
               <div>
-                <h2 class="pets_nome">${item.nome}</h2>
-                <ul class="pets_infos">
+                <h2 class="pets__name">${item.nome}</h2>
+                <ul class="pets__infos">
                   <li>${item.idade}</li>
                   <li>${item.porte}</li>
                   <li>${item.comportamento}</li>
                 </ul>
               </div>
               <div>
-                <p class="pets_endereco">${item.endereco}</p>
-                <a class="pets_responsavel" href="${item.contato}">Falar com responsável</a>
+                <p class="pets__address">${item.endereco}</p>
+                <a class="pets__responsible" href="${item.contato}">Falar com responsável</a>
               </div>
             </div>
           </li>`;
-}).join('');
+  })
+  .join("");
 
 conteinerPets.innerHTML = listaCompleta;
