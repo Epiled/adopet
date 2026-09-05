e1028793de4c5c41fa2c34b5ad5a358b4db1dc5d
Felipe De Andrade <felipe.deam98@gmail.com>
2026-09-05 00:35:42 -0300

    fix: handle missing pet list element

--- src/js/list.js
+++ src/js/list.js
@@ -2,25 +2,29 @@ import list from "./mocks/pets.js";
 
 const containerPets = document.querySelector("[data-list-pets]");
 
+if (!containerPets) {
+  throw new Error("Pets container not found");
+}
+
 const listRender = list
   .map((item) => {
     return `<li class="pets__item">
-            <img class="pets__image" src="./assets/img/pets/${item.image}" width="650" height="650" alt="${item.description}">
-            <div class="pets__data">
-              <div>
-                <h2 class="pets__name">${item.name}</h2>
-                <ul class="pets__infos">
-                  <li>${item.age}</li>
-                  <li>${item.size}</li>
-                  <li>${item.behavior}</li>
-                </ul>
+              <img class="pets__image" src="./assets/img/pets/${item.image}" width="650" height="650" alt="${item.description}">
+              <div class="pets__data">
+                <div>
+                  <h2 class="pets__name">${item.name}</h2>
+                  <ul class="pets__infos">
+                    <li>${item.age}</li>
+                    <li>${item.size}</li>
+                    <li>${item.behavior}</li>
+                  </ul>
+                </div>
+                <div>
+                  <p class="pets__address">${item.address}</p>
+                  <a class="pets__responsible" href="${item.contact}">Falar com responsável</a>
+                </div>
               </div>
-              <div>
-                <p class="pets__address">${item.address}</p>
-                <a class="pets__responsible" href="${item.contact}">Falar com responsável</a>
-              </div>
-            </div>
-          </li>`;
+            </li>`;
   })
   .join("");
 
